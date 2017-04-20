d83029debd31b518d6c2d3de9ceb258222a494cc
“sstone72389” <sstone72389@gmail.com>
2017-04-20 11:09:58 -0400

    Completes Enumerable training
    Completes training

--- bin/hash_list.rb
+++ bin/hash_list.rb
@@ -1,3 +1,27 @@
 # frozen_string_literal: true
 
 # hash as enumerable code along
+alpha = 'a'..'z'
+
+# cannot use bing in this case (you can with arrays)
+alpha = alpha.map(&:to_sym)
+
+# create new hash
+hash = {}
+
+# assigns alpha to first index which we indicated will be one
+# hash, alpha and index have all been defined
+
+(1..26).each_with_index do |number, index|
+  hash[alpha[index]] = number
+end
+
+# calling hash will return a key value pair of each symbol and its respective
+# place in the alphabet
+# you can now call methods on hash
+
+hash.all? do |_key, value|
+  value < 27
+end
+
+# will return true
--- bin/range_lab.rb
+++ bin/range_lab.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 # range as enumerable lab
+
+ints = 1..15
+
+even = ints.select { |num| num.even? }.reduce(:+)
+
+odd = ints.select { |num| num.odd? }.reduce(:+)
+
+# Even shorter way of writing it
+
+even = ints.select(&:even?).reduce(:+)
+
+odd = ints.select(&:odd?).reduce(:+)
